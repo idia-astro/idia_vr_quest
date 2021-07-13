3132f30e5c93fa9978504d229fa43e233d658131
veggiesaurus <accomrie@gmail.com>
2021-07-13 12:52:48 +0200

    reformatting

--- Tests/src/client.ts
+++ Tests/src/client.ts
@@ -1,40 +1,37 @@
-import { FileBrowserClient } from "../DataApi/DataApi_grpc_pb";
+import {FileBrowserClient} from "../DataApi/DataApi_grpc_pb";
 import * as grpc from "@grpc/grpc-js";
-import { FileListRequest, ImageInfoRequest } from "../DataApi/DataApi_pb";
+import {FileListRequest, ImageInfoRequest} from "../DataApi/DataApi_pb";
 
-const fileBrowserClient = new FileBrowserClient(
-  "localhost:50051",
-  grpc.credentials.createInsecure()
-);
+const fileBrowserClient = new FileBrowserClient("localhost:50051", grpc.credentials.createInsecure());
 
 const req = new FileListRequest();
 req.setDirectoryname("fits");
 
 fileBrowserClient.getFileList(req, (err, res) => {
-  if (err || !res) {
-    console.error(err);
-    return;
-  }
-
-  console.log(res.getDirectoryname());
-
-  const subdirs = res.getSubdirectoriesList();
-  const files = res.getFilesList();
-  for (const subdir of subdirs) {
-    console.log(`${subdir.getName()}: ${subdir.getNumitems()} items`);
-  }
-
-  for (const file of files) {
-    console.log(`${file.getName()}: ${file.getSize()} bytes`);
-    const req = new ImageInfoRequest();
-    req.setDirectoryname(res.getDirectoryname());
-    req.setFilename(file.getName());
-    fileBrowserClient.getImageInfo(req, (err1, res1) => {
-      if (err1) {
+    if (err || !res) {
         console.error(err);
-      } else if (res1) {
-        console.log(`${res1.getFilename()}: ${res1.getFiletype()}`);
-      }
-    });
-  }
+        return;
+    }
+
+    console.log(res.getDirectoryname());
+
+    const subdirs = res.getSubdirectoriesList();
+    const files = res.getFilesList();
+    for (const subdir of subdirs) {
+        console.log(`${subdir.getName()}: ${subdir.getNumitems()} items`);
+    }
+
+    for (const file of files) {
+        console.log(`${file.getName()}: ${file.getSize()} bytes`);
+        const req = new ImageInfoRequest();
+        req.setDirectoryname(res.getDirectoryname());
+        req.setFilename(file.getName());
+        fileBrowserClient.getImageInfo(req, (err1, res1) => {
+            if (err1) {
+                console.error(err);
+            } else if (res1) {
+                console.log(`${res1.getFilename()}: ${res1.getFiletype()}`);
+            }
+        });
+    }
 });
--- Tests/src/server.ts
+++ Tests/src/server.ts
@@ -1,116 +1,89 @@
 import * as grpc from "@grpc/grpc-js";
 import * as fs from "fs/promises";
 import * as path from "path";
-import {
-  sendUnaryData,
-  ServerUnaryCall,
-  UntypedHandleCall,
-} from "@grpc/grpc-js";
-import {
-  FileBrowserService,
-  IFileBrowserServer,
-} from "../DataApi/DataApi_grpc_pb";
-import {
-  DirectoryInfo,
-  FileInfo,
-  FileList,
-  FileListRequest,
-  FileType,
-  ImageInfo,
-  ImageInfoRequest,
-} from "../DataApi/DataApi_pb";
-import { getMagicNumber } from "./util";
+import {sendUnaryData, ServerUnaryCall, UntypedHandleCall} from "@grpc/grpc-js";
+import {FileBrowserService, IFileBrowserServer} from "../DataApi/DataApi_grpc_pb";
+import {DirectoryInfo, FileInfo, FileList, FileListRequest, FileType, ImageInfo, ImageInfoRequest} from "../DataApi/DataApi_pb";
+import {getMagicNumber} from "./util";
 
 let basePath = process.argv[2];
 
 const fitsMagicNumber = 6001412985720217632n;
 
 class FileBrowserServer implements IFileBrowserServer {
-  getImageInfo = async (
-    call: ServerUnaryCall<ImageInfoRequest, ImageInfo>,
-    callback: sendUnaryData<ImageInfo>
-  ) => {
-    const directoryName = call.request.getDirectoryname();
-    const fileName = call.request.getFilename();
-    const filePath = path.join(basePath, directoryName, fileName);
+    getImageInfo = async (call: ServerUnaryCall<ImageInfoRequest, ImageInfo>, callback: sendUnaryData<ImageInfo>) => {
+        const directoryName = call.request.getDirectoryname();
+        const fileName = call.request.getFilename();
+        const filePath = path.join(basePath, directoryName, fileName);
 
-    try {
-      const fd = await fs.open(filePath, "r");
-      const magicNum = await getMagicNumber(fd);
-      const response = new ImageInfo();
-      response.setFilename(fileName);
-      response.setDimensionsList([0]);
-      response.setFiletype(
-        magicNum === fitsMagicNumber ? FileType.FITS : FileType.UNKNOWN
-      );
-      callback(null, response);
-      await fd.close();
-    } catch (err) {
-      const errResponse: Error = {
-        name: "File info error",
-        message: err,
-      };
-      callback(errResponse, null);
-    }
-  };
-
-  getFileList = async (
-    call: ServerUnaryCall<FileListRequest, FileList>,
-    callback: sendUnaryData<FileList>
-  ) => {
-    const directoryName = call.request.getDirectoryname();
-    const dirPath = path.join(basePath, directoryName);
-
-    const response = new FileList();
-    const subdirs = response.getSubdirectoriesList();
-    const files = response.getFilesList();
-    response.setDirectoryname(directoryName);
-    try {
-      const list = await fs.readdir(dirPath);
-      for (const f of list) {
-        const fullPath = path.join(dirPath, f);
-        const stat = await fs.stat(fullPath);
-        if (stat.isDirectory()) {
-          const subDir = await fs.readdir(fullPath);
-          const dirInfo = new DirectoryInfo();
-          dirInfo.setName(f);
-          dirInfo.setNumitems(subDir.length);
-          dirInfo.setDate(stat.mtime.getDate());
-          subdirs.push(dirInfo);
-        } else if (stat.isFile()) {
-          const fileInfo = new FileInfo();
-          fileInfo.setName(f);
-          fileInfo.setSize(stat.size);
-          fileInfo.setDate(stat.mtime.getDate());
-          files.push(fileInfo);
+        try {
+            const fd = await fs.open(filePath, "r");
+            const magicNum = await getMagicNumber(fd);
+            const response = new ImageInfo();
+            response.setFilename(fileName);
+            response.setDimensionsList([0]);
+            response.setFiletype(magicNum === fitsMagicNumber ? FileType.FITS : FileType.UNKNOWN);
+            callback(null, response);
+            await fd.close();
+        } catch (err) {
+            const errResponse: Error = {
+                name: "File info error",
+                message: err
+            };
+            callback(errResponse, null);
         }
-      }
-    } catch (err) {
-      console.log(err);
-      const errResponse: Error = {
-        name: "File list error",
-        message: err,
-      };
+    };
 
-      callback(errResponse, null);
-    }
-    callback(null, response);
-  };
+    getFileList = async (call: ServerUnaryCall<FileListRequest, FileList>, callback: sendUnaryData<FileList>) => {
+        const directoryName = call.request.getDirectoryname();
+        const dirPath = path.join(basePath, directoryName);
 
-  [name: string]: UntypedHandleCall;
+        const response = new FileList();
+        const subdirs = response.getSubdirectoriesList();
+        const files = response.getFilesList();
+        response.setDirectoryname(directoryName);
+        try {
+            const list = await fs.readdir(dirPath);
+            for (const f of list) {
+                const fullPath = path.join(dirPath, f);
+                const stat = await fs.stat(fullPath);
+                if (stat.isDirectory()) {
+                    const subDir = await fs.readdir(fullPath);
+                    const dirInfo = new DirectoryInfo();
+                    dirInfo.setName(f);
+                    dirInfo.setNumitems(subDir.length);
+                    dirInfo.setDate(stat.mtime.getDate());
+                    subdirs.push(dirInfo);
+                } else if (stat.isFile()) {
+                    const fileInfo = new FileInfo();
+                    fileInfo.setName(f);
+                    fileInfo.setSize(stat.size);
+                    fileInfo.setDate(stat.mtime.getDate());
+                    files.push(fileInfo);
+                }
+            }
+        } catch (err) {
+            console.log(err);
+            const errResponse: Error = {
+                name: "File list error",
+                message: err
+            };
+
+            callback(errResponse, null);
+        }
+        callback(null, response);
+    };
+
+    [name: string]: UntypedHandleCall;
 }
 
 const server = new grpc.Server();
 server.addService(FileBrowserService, new FileBrowserServer());
 
-server.bindAsync(
-  "0.0.0.0:50051",
-  grpc.ServerCredentials.createInsecure(),
-  () => {
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), () => {
     console.log("Starting gRPC server");
     server.start();
-  }
-);
+});
 
 //
 //
